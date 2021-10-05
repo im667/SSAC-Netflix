39b623544644b31405e79b134f036b8c66055630
kwon <koh9037@gmail.com>
2021-10-06 01:50:39 +0900

    Button Event

--- Netflix/ViewController.swift
+++ Netflix/ViewController.swift
@@ -12,6 +12,9 @@ class MainViewController: UIViewController, UITextFieldDelegate {
     //아울렛 변수가 들어가는 곳
     //컴파일지시에 메모리정책         ! 강제언래핑(옵셔널)
     @IBOutlet var EmailTextBox: UITextField!
+    
+    
+    
     @IBOutlet var pwdTextBox: UITextField!
     
     @IBOutlet var HiddenTextField1: UITextField!
@@ -24,13 +27,13 @@ class MainViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var ShowTextBoxswitchBtn: UISwitch!
     
    
+    @IBOutlet var SignUpBtn: UIButton!
     
     //viewDidLoad->화면로드 완료시 자동호출 되는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         EmailTextBox.backgroundColor = UIColor.systemGray2
-        
         
         EmailTextBox.keyboardType = UIKeyboardType.emailAddress
         
@@ -46,11 +49,13 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         
         ShowTextBoxswitchBtn.isOn = false
         
+        SignUpBtn.addTarget(self, action: #selector(SignUpBtn(_:)), for: .touchUpInside)
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.EmailTextBox.resignFirstResponder()
+        
         self.pwdTextBox.resignFirstResponder()
         self.HiddenTextField1.resignFirstResponder()
         self.HiddenTextField2.resignFirstResponder()
@@ -75,8 +80,19 @@ class MainViewController: UIViewController, UITextFieldDelegate {
             self.HiddenTextField2.isHidden = true
             self.HiddenTextField3.isHidden = true
         }
-        print("ok")
     }
     
+    @IBAction func SignUpBtn(_ sender: UIButton) {
+        let id:String = EmailTextBox.text!
+        let pwd:String = pwdTextBox.text!
+        
+        if (id == ""){
+      print("ID를 입력하세요")
+        } else {
+            print(" ID:\(id) \n PW:\(pwd)"
+            )
+        }
+        
+    }
 }
 
